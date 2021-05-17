e3a4f1b5d604776ad9d55d714bd015a4ef6f70a7
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2021-05-18 00:03:19 +0530

    Update out.py

--- out.py
+++ out.py
@@ -1,7 +1,5 @@
 #after training the bot
 #return an output to the user
+#use get_response function to feed an input to Alpha and consequently return an output
 
-string = input("Enter what you want to say to that bot:")
-
-#use get_response function to feed an input to Alpha and consequrntly return an output
-print(bot.get_response(string))
+print(MyBot.get_response("Hello"))
