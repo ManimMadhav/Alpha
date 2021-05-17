ca14d3613d7968176557018be327e119ffed5c1d
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2021-05-17 23:30:17 +0530

    Update import-stuff.py

--- import-stuff.py
+++ import-stuff.py
@@ -1,4 +1,10 @@
 #import dependancies
+
+#import spacy, a module used by chatterbot
+import spacy
+spacy.cli.download("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm')
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
