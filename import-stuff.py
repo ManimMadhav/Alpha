8e7d9c3cce73d94e3cde9324162d81f2b8a107e9
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2021-05-19 00:33:19 +0530

    Update import-stuff.py

--- import-stuff.py
+++ import-stuff.py
@@ -1,17 +1,7 @@
-#import spacy, a module used by chatterbot
-#spaCy is a NLP library in python
-import spacy
-spacy.cli.download("en_core_web_sm")
-nlp = spacy.load('en_core_web_sm')
+#Natural Language Toolkit is a Python library
+#for NLP using Python
+import nltk
 
-
-#import dependancies
-#ChatterBot is a Python library that generates automated responses to a user's input
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-
-
-#creating an instance of class ChatBot
-
-#BestMatch helps Alpha select a proper response from the given list of responses
-mybot = ChatBot(name='Alpha', read_only = True, logic_adapters = ['chatterbot.logic.BestMatch'])
+#chat is a class for providing logic to responses
+#reflections is a dictionary that contains input & output responses
+from nltk.chat.util import Chat, reflections
